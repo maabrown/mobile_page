e6b39d2d2cf2c0937045f58c40a0471421a314d8
Mark Brown II <markbrown09@gmail.com>
2015-10-19 21:25:26 -0400

    Changed images for background and pizza changes with slider

--- views/js/main.js
+++ views/js/main.js
@@ -378,7 +378,7 @@ var pizzaElementGenerator = function(i) {
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.classList.add("col-md-6");
 
-  pizzaImage.src = "images/pizzaResized.png";
+  pizzaImage.src = "images/pizza-1.png";
   pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
