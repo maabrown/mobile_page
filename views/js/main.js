f7b6df62297ae4207fd574beee32d7e0cdc5e3f2
Mark Brown II <markbrown09@gmail.com>
2015-10-19 21:20:51 -0400

    Changed the image sizes for optimization

--- views/js/main.js
+++ views/js/main.js
@@ -378,7 +378,7 @@ var pizzaElementGenerator = function(i) {
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.classList.add("col-md-6");
 
-  pizzaImage.src = "images/pizza-1.png";
+  pizzaImage.src = "images/pizzaResized.png";
   pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
@@ -598,7 +598,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza2.png";
 
     //elem.style.height = "100px";
     //elem.style.width = "73.333px";
